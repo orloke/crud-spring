1f7768b7c52afc84d109e17d0371eccf9f9fb33f
Junior Dering <juniordering@hotmail.com>
2022-11-29 20:51:54 -0400

    building api

--- src/main/java/com/juniordering/crudspring/CrudSpringApplication.java
+++ src/main/java/com/juniordering/crudspring/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.juniordering.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.juniordering.crudspring.model.Course;
+import com.juniordering.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Front-End");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
